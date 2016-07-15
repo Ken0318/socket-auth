d3df27376db94f3323277c020433e4b3c8f8ec3a
yoni hao <yoni hao>
2016-07-15 23:30:24 +0800

    modify client.c type remove client2.c
    Signed-off-by: xinkai
    Review-by: ken

--- src/client.c
+++ src/client.c
@@ -2,44 +2,58 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
+#include <Sha256Calc.h>
 
 #define MAXDATASIZE 1024
 
 #define SERVERIP "192.168.7.2" 
 #define SERVERPORT 8000
+#define AUTH_FILE "/home/auth.key"
 
-int main( int argc, char * argv[ ] ) 
+int main(int argc, char **argv) 
 { 
     char buf[ MAXDATASIZE] ; 
-    char buf2[ MAXDATASIZE] = "Hello hangzhou"; 
     int sockfd, numbytes; 
     struct sockaddr_in server_addr; 
-    if ( ( sockfd = socket ( AF_INET , SOCK_STREAM , 0) ) == -1) { 
-        perror ( "socket error" ) ; 
+    Sha256Calc  shac;
+    char enc_data[256] = {0};
+ 
+    FILE *fp;
+    fp = fopen(AUTH_FILE, "r");
+    if (fp == NULL)
+        return 1;
+    while(fgets(enc_data, 255, fp) != NULL) {}
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0) ) == -1) { 
+        perror("socket error") ; 
         return 1; 
     } 
-    memset ( &server_addr, 0, sizeof ( struct sockaddr ) ) ; 
+
+    memset(&server_addr, 0, sizeof(struct sockaddr)) ; 
     server_addr.sin_family = AF_INET ; 
     server_addr.sin_port = htons ( SERVERPORT) ; 
     server_addr.sin_addr.s_addr = inet_addr( SERVERIP) ; 
-    if ( connect ( sockfd, ( struct sockaddr * ) &server_addr, sizeof ( struct sockaddr ) ) == -1) { 
-        perror ( "connect error" ) ; 
+    if (connect (sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) { 
+        perror("connect error") ; 
         return 1; 
     } 
-//    printf ( "send: Hello, world!/n" ) ; 
-    
-    if ( send ( sockfd, buf2 , sizeof(buf2), 0) == -1) { 
-        perror ( "send error" ) ; 
-        return 1; 
-    } 
-    if ( ( numbytes = recv ( sockfd, buf, MAXDATASIZE, 0) ) == -1) { 
+
+    Sha256Calc_reset(&shac);
+    Sha256Calc_calculate(&shac, (SZ_CHAR*) enc_data, strlen(enc_data));
+    if (send(sockfd, &shac.Value, strlen(enc_data), 0) == -1) {
+        perror ( "send error" ) ;
+        return 1;
+    }
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE, 0)) == -1) { 
         perror ( "recv error" ) ; 
         return 1; 
     } 
-    if ( numbytes) { 
+
+    if (numbytes) { 
         buf[ numbytes] = '\0' ; 
-        printf ( "received: %s\n" , buf) ; 
     } 
-    close ( sockfd) ; 
+
+    close(sockfd) ; 
     return 0; 
 }
